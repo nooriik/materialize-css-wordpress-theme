fc83f5e1b3119b8018f913543a5ecd89edb5c1cb
Noor Ullah <visaab9002@gmail.com>
2017-07-01 02:44:30 -0700

    Updated Version Of Header.php

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		    <nav class="nav-extended">
 		        <div class="nav-wrapper">		        	
-				    <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="brand-logo center">
+				    <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="brand-logo">
 					    <?php
 			        		$custom_logo_id = get_theme_mod( 'custom_logo' );
 			        		if($custom_logo_id)
@@ -39,22 +39,16 @@
 			        	?>	        	
 				    </a>
 					<ul id="nav-mobile" class="right hide-on-med-and-down">
-						<?php wp_nav_menu( array('theme_location' => 'secondary')); ?>
+						<?php wp_nav_menu( array('theme_location' => 'primary')); ?>
 					</ul>
 		
 			    	<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 
 				    <div class="side-nav" id="mobile-demo">
-						    <h5 class="center mobile-title"><?php bloginfo('name'); ?></h5>
-				        	<?php wp_nav_menu( array('theme_location' => 'secondary')); ?>
+						<h5 class="center mobile-title"><?php bloginfo('name'); ?></h5>
 				        	<?php wp_nav_menu( array( 'theme_location' => 'primary')); ?>	
 			        </div>
 		    	</div>
-		      <div class="nav-content hide-on-med-and-down">
-		        <ul>
-					<?php wp_nav_menu( array('theme_location' => 'primary')); ?>
-		        </ul>
-		      </div>
 		    </nav>
 	</header><!-- #masthead -->
 	<div id="content" class="row">
